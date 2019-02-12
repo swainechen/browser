80e6345ce49cc51d8634e853f16340b23ae9e45a
root <root@ip-10-0-2-203.ap-southeast-1.compute.internal>
2019-02-12 04:26:54 +0000

    Add -makedir to create skeleton directory from URL in ReferenceGenomes table

--- bin/mysql-get-reference.pl
+++ bin/mysql-get-reference.pl
@@ -1,12 +1,12 @@
-#!/usr/bin/perl -w
-#
-# Take in a run number
+#!/usr/bin/perl -w # # Take in a run number
 # Print out species, reference sequence
 #
 use warnings;
 use strict;
 use File::Spec;
 use File::Basename;
+use File::Path;
+use File::Fetch;
 use GERMS;
 use DBI;
 use Getopt::Long;
@@ -16,20 +16,23 @@ my $quiet = 1;
 my $force_species = "NONE";
 my $force_reference = "NONE";
 my $OUTBREAK_BASE = $ENV{'OUTBREAK_BASE'};
+my $makedir = 1;
 GetOptions (
   'quiet!' => \$quiet,
   'species=s' => \$force_species,
-  'reference=s' => \$force_reference
+  'reference=s' => \$force_reference,
+  'makedir!' => \$makedir
 );
 
 if (!defined $ARGV[0] || !length($ARGV[0]) || !defined $OUTBREAK_BASE || -f $OUTBREAK_BASE) {
-  print "Usage: $0 <Run ID> [ -species <species> ] [ -reference <reference> ]\n";
+  print "Usage: $0 <Run ID> [ -species <species> ] [ -reference <reference> ] [ -makedir|nomakedir ]\n";
   print "Will print out:\n";
   print "  Directory for rest of files\n";
   print "  Species (abbreviation) for SRST2\n";
   print "  Reference sequence for mapping\n";
   print "Can force Species, use \"Ecoli\" or \"Escherichia coli\" (quoted as needed)\n";
   print "Can force Reference, use Reference filename, Reference md5sum, Reference genome name (like \"EC958\" or \"LT2\")\n";
+  print "With -makedir will make the directories and download and index necessary references in OUTBREAK_BASE\n";
   if (!defined $OUTBREAK_BASE || -f $OUTBREAK_BASE) {
     print "-- Error, OUTBREAK_BASE environment variable not set\n";
   }
@@ -42,10 +45,12 @@ my $directory = "";
 my $fullspecies = "";	# this is like "Escherichia coli"
 my $species = "";	# this is like "Ecoli"
 my $reference = "";
+my $URL = "";
 my $dbh = GERMS::dbconnect("germs_browser");
 my $sql;
 my $sth;
 my $temp;
+my $ff;
 
 if (uc($force_species) eq "NONE") {
   $sql = "SELECT Kraken FROM Fastq JOIN Tips ON Fastq.TIP = Tips.TIP WHERE Run = ?";
@@ -90,42 +95,74 @@ if (length $fullspecies) {
 }
 
 if (uc($force_reference) eq "NONE") {
-  $sql = "SELECT ReferenceFile FROM ReferenceGenomes WHERE DefaultReference = 1 AND Species = ?";
+  $sql = "SELECT ReferenceFile, URL FROM ReferenceGenomes WHERE DefaultReference = 1 AND Species = ?";
   $sth = $dbh->prepare($sql);
   $sth->execute($species);
   while (@data = $sth->fetchrow_array()) {
-  if (length $data[0]) {
-      if (-f $data[0]) {
-        $reference = File::Spec->rel2abs($data[0]);
-      } elsif (-f "$OUTBREAK_BASE/$data[0]") {
-        $reference = File::Spec->rel2abs("$OUTBREAK_BASE/$data[0]");
-      }
+    if (length $data[0]) {
+      $reference = $data[0];
+      $URL = $data[1];
       last;
     }
   }
 } else {
-  if (-f $force_reference) {
+  # this should enable using just the base file name, md5sum,
+  # or reference name as $force_reference
+  $species = "";
+  $sql = "SELECT Species, ReferenceFile, URL from ReferenceGenomes WHERE ReferenceFile LIKE '%?' OR ReferenceMD5 = ? OR ReferenceName = ?";
+  $sth = $dbh->prepare($sql);
+  $sth->execute("%" . $force_reference, $force_reference, $force_reference);
+  while (@data = $sth->fetchrow_array()) {
+    $species = $data[0];
+    $reference = $data[1];
+    $URL = $data[2];
+    last;
+  }
+  if ($species eq "" && -f $force_reference) {
     $reference = File::Spec->rel2abs($force_reference);
-  } else {
-    # this should enable using just the base file name, md5sum,
-    # or reference name as $force_reference
-    $sql = "SELECT Species, ReferenceFile from ReferenceGenomes WHERE ReferenceFile LIKE ? OR ReferenceMD5 = ? OR ReferenceName = ?";
-    $sth = $dbh->prepare($sql);
-    $sth->execute("%" . $force_reference, $force_reference, $force_reference);
-    while (@data = $sth->fetchrow_array()) {
-      if (length $data[0] && length $data[1]) {
-        $species = $data[0];
-        $reference = File::Spec->rel2abs($data[1]);
-        last;
-      }
+  }
+}
+
+# check for files, make dirs and download if needed
+if (-f $reference) {
+  $reference = File::Spec->rel2abs($reference);
+  $directory = File::Basename::dirname($reference);
+} elsif (-f "$OUTBREAK_BASE/$reference") {
+  $reference = File::Spec->rel2abs("$OUTBREAK_BASE/$reference");
+  $directory = File::Basename::dirname($reference);
+} elsif ($reference ne "" && $reference ne "NONE") {
+  $reference =~ s/^$OUTBREAK_BASE\///;
+  $directory = "$OUTBREAK_BASE/" . File::Basename::dirname($reference);
+  if ($makedir && defined $URL && length($URL)) {
+    if (-d $directory) {
+      File::Path::make_path($directory) || die "Cannot make $directory and -makedir is true\n";
+    }
+    $ff = File::Fetch->new( uri => $URL );
+    $ff->fetch( to => $directory );
+    if (!defined $ff->output_file || !-f "$directory/" . $ff->output_file) {
+      die "Cannot download $URL (to $directory/" . $ff->output_file . ") and -makedir is true\n";
+    }
+    if ($ff->output_file =~ /\.gz$/) {
+      system("gunzip $directory/" . $ff->output_file);
+    }
+    if (-f "$OUTBREAK_BASE/$reference") {
+      $reference = File::Spec->rel2abs("$OUTBREAK_BASE/$reference");
+      system("samtools faidx $reference");
+      system("bwa index $reference");
+    } else {
+      die "Error, seemed to download $URL ok to $directory but can't find $OUTBREAK_BASE/$reference\n";
     }
   }
 }
-if (length $reference && -f $reference) {
-  $directory = File::Basename::dirname($reference);
-} else {
+if (!-f $reference) {
   $reference = "NONE";
   $directory = "$OUTBREAK_BASE/Others/Noref";
+  if (!-d $directory && $makedir) {
+    File::Path::make_path($directory);
+    if (!-d $directory) {
+      die "Cannot make $directory and -makedir is true\n";
+    }
+  }
 }
 
 if (!defined $species || $species eq "") {
@@ -134,6 +171,7 @@ if (!defined $species || $species eq "") {
 if (!defined $fullspecies || $fullspecies eq "") {
   $fullspecies = "NONE";
 }
+
 print "WORK=$directory\n";
 print "REF=$reference\n";
 print "SPECIES=$species\n";
