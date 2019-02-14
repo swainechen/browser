d9a40c972aad6d07df328b51b0cf78d77e494ed5
Ubuntu <ubuntu@ip-10-0-2-203.ap-southeast-1.compute.internal>
2019-02-14 06:42:14 +0000

    Add -download option, deal with not having genomes for a given species better by pulling them from prokaryotes.csv

--- bin/closest_species.pl
+++ bin/closest_species.pl
@@ -4,29 +4,58 @@ use warnings;
 use GERMS;
 use Orgmap;
 use File::Temp;
+use File::Fetch;
+use File::Basename;
 use Getopt::Long;
 Getopt::Long::Configure("pass_through");
 
+# this file comes from http://www.ncbi.nlm.nih.gov/genome/browse/#
+# currently (14 Feb 2019) the fields are (with examples, quotes removed):
+# 0 - Organism Name: Corynebacterium jeikeium K411
+# 1 - Organism Groups: Bacteria;Terrabacteria group;Actinobacteria
+# 2 - Strain: K411 = NCTC 11915
+# 3 - BioSample: SAMEA3283089
+# 4 - BioProject: PRJNA13967
+# 5 - Assembly: GCA_000006605.1
+# 6 - Level: Complete
+# 7 - Size(Mb): 2.47682
+# 8 - GC%: 61.3561
+# 9 - Replicons: chromosome:NC_007164.1/CR931997.1; plasmid pKW4:NC_003080.1/AF401314.1
+# 10 - WGS:
+# 11 - Scaffolds: 2
+# 12 - CDS: 2088
+# 13 - Release Date: 2005-06-27T00:00:00Z
+# 14 - GenBank FTP: ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/006/605/GCA_000006605.1_ASM660v1
+# 15 - RefSeq FTP: ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/006/605/GCF_000006605.1_ASM660v1
+my $PROKS_CSV = "/mnt/volume1/fastq/prokaryotes.csv";
+# number of genomes to get on the fly from the above file if needed
+my $NEW_DOWNLOAD = 10;
+my $DOWNLOAD = "";
 my $debug = 0;
 my $num_test = 200;
 my $initial = "";
 GetOptions (
   'initial=s' => \$initial,
   'num_test=i' => \$num_test,
+  'download=s' => \$DOWNLOAD,	# where to leave downloaded genomes
   'debug' => \$debug
 );
 
 if (!defined $ARGV[0] || !-f $ARGV[0]) {
-  print "Usage: $0 [ -initial <string> ] [ -num_test <int> ] <fasta file>\n";
+  print "Usage: $0 [ -initial <string> ] [ -num_test <int> ] [ -download <path> ] <fasta file>\n";
   print "Will give a list, one per line, of .fna files that can be used collectively\n";
   print "as a species reference for the input fasta file (for novel sequence calling,\n";
   print "etc.  The file first on the list is the one that is closest to the input file.\n";
   print "-initial specifies an initial species ID (such as from Kraken or an orgcode) so we can skip the initial blast against the reduced species database\n";
   print "-num_test is how many of the longest sequences to test. Intended to be something like N50 number or similar. Defaults to 200.\n";
+  print "-download specifies a download directory for new genomes. If specified, these genomes will be left there and not cleaned up after program execution.\n";
   exit;
 }
 
-my $reduced = "/mnt/genomeDB/ncbi/genomes/Bacteria/bacteria-genus.fna";
+if ($initial eq "") {
+  # we can't really do anything at this point
+  exit;
+}
 my $tempdir;
 if ($debug) {
   $tempdir = File::Temp::tempdir( CLEANUP => 0 );
@@ -35,11 +64,6 @@ if ($debug) {
   $tempdir = File::Temp::tempdir( CLEANUP => 1 );
 }
 my $tempfile = "__closest_species.tmp";
-
-if (!-f $reduced) {
-  print "Can't find reduced fna file $reduced.  Please regenerate with make-reduced-fna.pl\n";
-  die;
-}
 
 # the program itself...
 print join ("\n", closest_species($ARGV[0], $num_test, $initial)), "\n";
@@ -67,10 +91,7 @@ sub closest_species {
   if (scalar @reference == 0) {
     print STDERR "Couldn't find any reference sequences for initial organism $initial\n" if $debug && $organism ne "";
     $organism = "";
-    print STDERR "Starting blast against reduced fna file\n" if $debug;
-    ($organism, $best_orgcode) = blast_assembly($assembly, $num_test, $reduced);
-    print STDERR "Return from reduced fna blast\n" if $debug;
-    print STDERR "organism: $organism\nbest_orgcode: $best_orgcode\n" if $debug;
+    exit;
   }
 
   if ($organism ne "" && $best_orgcode ne "") {
@@ -161,9 +182,23 @@ sub get_species {
 sub fna_files {
   # we get an organism designation, maybe genus or genus/species or orgcode
   # return a list of all the .fna files in an array
+  # The database of fna files used to be at ftp://ftp.ncbi.nih.gov/genomes/Bacteria/
+  # Now seems to be at ftp://ftp.ncbi.nih.gov/genomes/refseq/bacteria but follows the GCF/GCA designation now
+  # In any case, it's huge...only have a few downloaded
+  # If we don't have anything downloaded here then go to prokaryotes.csv file to fetch some on the fly
   my ($organism) = @_;
   my @f;
+  my @g;
   my @files = ();
+  my $ff;
+  my $url;
+  my $tempfile;
+  my $output_dir;
+  if ($DOWNLOAD eq "") {
+    $output_dir = $tempdir;
+  } else {
+    $output_dir = $DOWNLOAD;
+  }
   open ORG, "$Orgmap::LIBPATH/org-map";
   while ($line = <ORG>) {
     next if $line =~ /^$/;
@@ -175,6 +210,41 @@ sub fna_files {
     }
   }
   close ORG;
+  if (!scalar @files && -f $PROKS_CSV) {
+    open F, $PROKS_CSV;
+    while (<F>) {
+      chomp;
+      next if /^#/;
+      @f = split /,/, $_;
+      foreach $i (0..$#f) {
+        $f[$i] =~ s/^"//;
+        $f[$i] =~ s/"$//;
+      }
+      next if $f[6] ne "Complete";
+      if ($f[0] =~ /^$organism/) {
+        $tempfile = "";
+        $url = $f[15];
+        $url .= "/" . File::Basename::basename($url) . "_genomic.fna.gz";
+        $ff = File::Fetch->new( uri => $url );
+        $ff->fetch( to => $output_dir );
+        if (-f $ff->output_file) {
+          $tempfile = File::Spec->rel2abs($ff->output_file);
+        } elsif (-f "$output_dir/" . $ff->output_file) {
+          $tempfile = File::Spec->rel2abs("$output_dir/" . $ff->output_file);
+        }
+        if ($tempfile ne "") {
+          if ($tempfile =~ /\.gz$/) {
+            system "gunzip $tempfile";
+            $tempfile =~ s/\.gz$//;
+            push @files, $tempfile;
+            if (scalar(@files) >= $NEW_DOWNLOAD) {
+              return @files;
+            }
+          }
+        }
+      }
+    }
+  }
   return @files;
 }
 
