2d03e55553b19666f503efca05cc36dd03638fcd
root <root@ip-172-31-0-136.ap-southeast-1.compute.internal>
2022-08-15 08:08:41 +0000

    fix ordering refs by size modifications to windowing to decrease number of points for long sequences and increase it for short references

--- bin/gcov-graph.R
+++ bin/gcov-graph.R
@@ -7,8 +7,8 @@ if (length(args) < 1 | "--help" %in% args) {
   q()
 }
 
-window <- 1000
-dither <- 1000
+maxpoints <- 1000
+maxwindow <- 1000
 for(i in 1:length(args)) {
   if (file.exists(args[i])) {
     if (grepl("gcov.gz$", args[i])) {
@@ -17,19 +17,22 @@ for(i in 1:length(args)) {
       run <- sub(".gcov.gz$", "", args[i])
     } else {
       cov <- read.table(args[i])
+      run <- sub(".gcov$", "", args[i])
       output <- paste(args[i], ".png", sep="")
     }
     png(output, width=1024, height=512)
     ref <- unique(cov$V1)
     refsize <- rep(0, length(ref))
-    ref <- ref[order(refsize, decreasing=T)]
-    refsize <- refsize[order(refsize, decreasing=T)]
     cov$filter = 0;
     for(j in 1:length(ref)) {
       refsize[j] <- max(cov$V2[cov$V1==ref[j]])
-      window <- min(1000, refsize[j]-1)
+      window <- max(1, floor(refsize[j]/maxpoints))
+      if (window > maxwindow) window <- maxwindow
       cov$filter[cov$V1==ref[j]] <- filter(cov$V3[cov$V1==ref[j]], rep(1/(window+1), window+1), sides=2)
     }
+    reforder <- order(refsize, decreasing=T)
+    ref <- ref[reforder]
+    refsize <- refsize[reforder]
     size <- round(log10(refsize))
     size[1] <- size[1] + 1.5
     ifelse(length(ref) > 1, linepos <- rep(1, length(ref)), linepos <- 0.5)
@@ -39,7 +42,7 @@ for(i in 1:length(args)) {
       ifelse(j==1, par(mar=c(0,2,0,0)), ifelse(j==length(ref), par(mar=c(0,0,0,2)), par(mar=c(0,0,0,0))))
       tx <- cov$V2[cov$V1==ref[j]]
       ty <- cov$filter[cov$V1==ref[j]]
-      toplot <- seq(1, length(tx), dither)
+      toplot <- seq(1, length(tx), window)
       plot(tx[toplot], log10(ty[toplot]+1), type='l', lwd=1, ylim=log10(range(cov$V3)+1), xaxt="n", yaxt="n", axes=F)
        if (j %% 2 == 1) {
          mtext(paste(sep="", "Reference: \n", ref[j], "(", max(tx), " nt)"), side=3, cex=1.25, line=0.5)
@@ -50,7 +53,7 @@ for(i in 1:length(args)) {
        }
        ifelse(j==1, axis(2, ylim=log10(range(cov$V3)+1), cex.axis=1), ifelse(j==length(ref), axis(4, ylim=log10(range(cov$V3)+1), cex.axis=1), NA))
     }
-    ydetail <- paste(sep="", "\n(sliding window of size ", window, " bp, step size ", dither, " bp)")
+    ydetail <- paste(sep="", "\n(sliding window of size ", window, " bp, step size ", window, " bp)")
     mtext(paste(sep="", "Log10 coverage", ydetail), side=2, line=1, cex=1, outer=TRUE)
     if(length(ref) > 1) mtext("Log10 coverage", side=4, line=1.5, cex=1, outer=TRUE)
     mtext(paste(sep="", "Coverage plot (", run, ")"), side=3, line=3.5, outer=T, cex=1.75)
